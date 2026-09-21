522aa4b0de55e8bf27ed17c127238be63f2c4c8a
Bxrtxrr <67070277@kmitl.ac.th>
2026-09-21 23:46:51 +0700

    ex03

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,9 +1,10 @@
 num1 = input("Enter the first number: ")
 num2 = input("Enter the second number: ")
-print(f"{num1} x {num2} = {int(num1) * int(num2)}")
-if int(num1) * int(num2) > 0:
+result = int(num1) * int(num2)
+print(f"{num1} x {num2} = {result}")
+if result > 0:
     print("The result is positive.")
-elif int(num1) * int(num2) < 0:
+elif result < 0:
     print("The result is negative.")
 else:
     print("The result is zero.")
